2679188dc92f3b234b44392316d363f41c2ad97f
Kark Li <kark_li@qq.com>
2024-06-11 19:11:44 +0800

    upload repeated data's benchmark test code

--- codec_benchmark_test.go
+++ codec_benchmark_test.go
@@ -1,14 +1,16 @@
 package codec
 
 import (
+	"math"
 	"math/rand"
 	"testing"
 
 	"github.com/KarKLi/protobuf-golang-codec/internal/proto3_test"
+	"google.golang.org/protobuf/encoding/protowire"
 	"google.golang.org/protobuf/proto"
 )
 
-var bin []byte
+var testBin []byte
 
 var testMsg = &proto3_test.Msg{
 	I_1:  -rand.Int31(),
@@ -35,18 +37,58 @@ var testMsg = &proto3_test.Msg{
 	F_17: -rand.Float32(),
 }
 
+var testPackedRepeatedBin []byte
+
+var testPackedRepeatedMsg = &proto3_test.RepeatedMsgWithPacked{
+	I_1:  []int32{0, math.MaxInt32, rand.Int31(), -rand.Int31()},
+	I_2:  []int64{0, math.MaxInt64, rand.Int63(), -rand.Int63()},
+	U_3:  []uint32{0, math.MaxInt32, rand.Uint32()},
+	U_4:  []uint64{0, math.MaxInt64, rand.Uint64()},
+	S_5:  []int32{0, math.MaxInt32, rand.Int31(), -rand.Int31()},
+	S_6:  []int64{0, math.MaxInt64, rand.Int63(), -rand.Int63()},
+	B_7:  []bool{true, false, true, true, false},
+	E_8:  []proto3_test.TestEnum{proto3_test.TestEnum_ZERO, proto3_test.TestEnum_ONE, proto3_test.TestEnum_TWO},
+	F_9:  []uint64{0, math.MaxInt64, rand.Uint64()},
+	S_10: []int64{0, math.MaxInt64, rand.Int63(), -rand.Int63()},
+	D_11: []float64{0, math.MaxFloat64, rand.Float64(), -rand.Float64()},
+	F_12: []uint32{0, math.MaxInt32, rand.Uint32()},
+	S_13: []int32{0, math.MaxInt32, rand.Int31(), -rand.Int31()},
+	F_14: []float32{0, math.MaxFloat32, rand.Float32(), -rand.Float32()},
+}
+
+var testPackedRepeatedDecoderMap = map[protowire.Number]packedRepeatedDecoder{
+	1:  PackedRepeatedInt32Decoder,
+	2:  PackedRepeatedInt64Decoder,
+	3:  PackedRepeatedUint32Decoder,
+	4:  PackedRepeatedUint64Decoder,
+	5:  PackedRepeatedSint32Decoder,
+	6:  PackedRepeatedSint64Decoder,
+	7:  PackedRepeatedBoolDecoder,
+	8:  PackedRepeatedEnumDecoder,
+	9:  PackedRepeatedFixed64Decoder,
+	10: PackedRepeatedSfixed64Decoder,
+	11: PackedRepeatedDoubleDecoder,
+	12: PackedRepeatedFixed32Decoder,
+	13: PackedRepeatedSfixed32Decoder,
+	14: PackedRepeatedFloatDecoder,
+}
+
 func initTestData(b *testing.B) {
 	var err error
-	bin, err = proto.Marshal(testMsg)
+	testBin, err = proto.Marshal(testMsg)
 	if err != nil {
-		b.Fatalf("can not marshal test proto message, err: %+v", err)
+		b.Fatalf("can not marshal Msg message, err: %+v", err)
+	}
+	testPackedRepeatedBin, err = proto.Marshal(testPackedRepeatedMsg)
+	if err != nil {
+		b.Fatalf("can not marshal RepeatedMsgWithPacked message, err: %+v", err)
 	}
 }
 
-func BenchmarkBaseline(b *testing.B) {
+func BenchmarkNonRepeatedBaseline(b *testing.B) {
 	initTestData(b)
 	for i := 0; i < b.N; i++ {
-		if err := proto.Unmarshal(bin, &proto3_test.Msg{}); err != nil {
+		if err := proto.Unmarshal(testBin, &proto3_test.Msg{}); err != nil {
 			b.Fatalf("can not unmarshal test proto message, err: %+v", err)
 		}
 	}
@@ -56,10 +98,38 @@ func BenchmarkDecodeNonRepeatedData(b *testing.B) {
 	initTestData(b)
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		m, err := Decode(bin, NotSort)
+		m, err := Decode(testBin, NotSort)
 		if err != nil {
-			b.Fatalf("decode test proto message into map failed, err: %+v", err)
+			b.Fatalf("decode Msg proto message into ProtoMessage struct failed, err: %+v", err)
 		}
 		_ = m
+	}
+}
+
+func BenchmarkRepeatedBaseline(b *testing.B) {
+	initTestData(b)
+	for i := 0; i < b.N; i++ {
+		if err := proto.Unmarshal(testPackedRepeatedBin, &proto3_test.RepeatedMsgWithPacked{}); err != nil {
+			b.Fatalf("can not unmarshal test proto message, err: %+v", err)
+		}
+	}
+}
+
+func BenchmarkDecodePackedRepeatedData(b *testing.B) {
+	initTestData(b)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		m, err := Decode(testPackedRepeatedBin, NotSort)
+		if err != nil {
+			b.Fatalf("decode RepeatedMsgWithPacked proto message into ProtoMessage struct failed, err: %+v", err)
+		}
+		// 对所有repeated字段继续解码
+		for i := range m.values {
+			tag := m.values[i].tag
+			_, err := m.DecodePackedRepeated(tag, testPackedRepeatedDecoderMap[tag])
+			if err != nil {
+				b.Fatalf("decode RepeatedMsgWithPacked %d field failed, err: %+v", tag, err)
+			}
+		}
 	}
 }
