8e09ba6d6d24d96ee3f77324118cf98debe7b7ad
Kark Li <kark_li@qq.com>
2024-06-11 19:28:16 +0800

    upload unpacked repeated data's benchmark test code

--- README.md
+++ README.md
@@ -72,14 +72,18 @@ goarch: amd64
 pkg: github.com/KarKLi/protobuf-golang-codec
 cpu: AMD EPYC 7K62 48-Core Processor
 BenchmarkNonRepeatedBaseline
-BenchmarkNonRepeatedBaseline-8        	 1484918	       801.1 ns/op	     304 B/op	       5 allocs/op
+BenchmarkNonRepeatedBaseline-8          	 1499984	       799.0 ns/op	     304 B/op	       5 allocs/op
 BenchmarkDecodeNonRepeatedData
-BenchmarkDecodeNonRepeatedData-8      	 1278822	       936.1 ns/op	    1696 B/op	      17 allocs/op
-BenchmarkRepeatedBaseline
-BenchmarkRepeatedBaseline-8           	  754969	      1517 ns/op	     677 B/op	      15 allocs/op
+BenchmarkDecodeNonRepeatedData-8        	 1274136	       957.5 ns/op	    1696 B/op	      17 allocs/op
+BenchmarkPackedRepeatedBaseline
+BenchmarkPackedRepeatedBaseline-8       	  752754	      1487 ns/op	     677 B/op	      15 allocs/op
 BenchmarkDecodePackedRepeatedData
-BenchmarkDecodePackedRepeatedData-8   	  258675	      4596 ns/op	    2144 B/op	      76 allocs/op
+BenchmarkDecodePackedRepeatedData-8     	  256388	      4605 ns/op	    2144 B/op	      76 allocs/op
+BenchmarkUnpackedRepeatedBaseline
+BenchmarkUnpackedRepeatedBaseline-8     	  763533	      1499 ns/op	     677 B/op	      15 allocs/op
+BenchmarkDecodeUnpackedRepeatedData
+BenchmarkDecodeUnpackedRepeatedData-8   	   16624	     72084 ns/op	   71776 B/op	     933 allocs/op
 PASS
-ok  	github.com/KarKLi/protobuf-golang-codec	6.559s
+ok  	github.com/KarKLi/protobuf-golang-codec	9.644s
 ```
 Benchmark code can be referenced at `codec_benchmark_test.go`.
--- codec_benchmark_test.go
+++ codec_benchmark_test.go
@@ -73,6 +73,101 @@ var testPackedRepeatedDecoderMap = map[protowire.Number]packedRepeatedDecoder{
 	14: PackedRepeatedFloatDecoder,
 }
 
+var testUnpackedRepeatedBin []byte
+
+var testUnpackedRepeatedMsg = &proto3_test.RepeatedMsgWithUnpacked{
+	I_1:  []int32{0, math.MaxInt32, rand.Int31(), -rand.Int31()},
+	I_2:  []int64{0, math.MaxInt64, rand.Int63(), -rand.Int63()},
+	U_3:  []uint32{0, math.MaxInt32, rand.Uint32()},
+	U_4:  []uint64{0, math.MaxInt64, rand.Uint64()},
+	S_5:  []int32{0, math.MaxInt32, rand.Int31(), -rand.Int31()},
+	S_6:  []int64{0, math.MaxInt64, rand.Int63(), -rand.Int63()},
+	B_7:  []bool{true, false, true, true, false},
+	E_8:  []proto3_test.TestEnum{proto3_test.TestEnum_ZERO, proto3_test.TestEnum_ONE, proto3_test.TestEnum_TWO},
+	F_9:  []uint64{0, math.MaxInt64, rand.Uint64()},
+	S_10: []int64{0, math.MaxInt64, rand.Int63(), -rand.Int63()},
+	D_11: []float64{0, math.MaxFloat64, rand.Float64(), -rand.Float64()},
+	F_12: []uint32{0, math.MaxInt32, rand.Uint32()},
+	S_13: []int32{0, math.MaxInt32, rand.Int31(), -rand.Int31()},
+	F_14: []float32{0, math.MaxFloat32, rand.Float32(), -rand.Float32()},
+	S_15: []string{"", "aaa", "bbbbb", "cc$%^", "123", "1-2+3", "你好"},
+	B_16: [][]byte{nil, []byte(""), []byte("aaa"), []byte("bbbbb"), []byte("cc$%^"), []byte("123"), []byte("1-2+3"), []byte("你好")},
+	M_17: []*proto3_test.Embeeded{
+		{},
+		{
+			I_1: rand.Int31(),
+			F_2: rand.Uint64(),
+			S_3: "aa",
+			F_4: rand.Uint32(),
+		},
+		{
+			I_1: rand.Int31(),
+			F_2: rand.Uint64(),
+			S_3: "",
+			F_4: rand.Uint32(),
+		},
+		{
+			I_1: -rand.Int31(),
+			F_2: rand.Uint64(),
+			S_3: "aa",
+			F_4: rand.Uint32(),
+		},
+		{
+			I_1: -rand.Int31(),
+			F_2: rand.Uint64(),
+			S_3: "",
+			F_4: rand.Uint32(),
+		},
+	},
+	M_18: map[int32]string{0: "", 1: "", 2: "aa", 3: "你好"},
+	M_19: map[string]int32{"": 0, "a": 1, "aa": 2, "你好": 3},
+	M_20: map[string]*proto3_test.Embeeded{
+		"":  {I_1: rand.Int31()},
+		"a": {},
+		"aa": {
+			I_1: rand.Int31(),
+			F_2: rand.Uint64(),
+			S_3: "aa",
+			F_4: rand.Uint32(),
+		},
+		"你好": {
+			I_1: rand.Int31(),
+			F_2: rand.Uint64(),
+			S_3: "",
+			F_4: rand.Uint32(),
+		},
+	},
+}
+
+var testUnpackedRepeatedDecoderMap = map[protowire.Number]unpackedRepeatedDecoder{
+	1:  UnpackedRepeatedInt32Decoder,
+	2:  UnpackedRepeatedInt64Decoder,
+	3:  UnpackedRepeatedUint32Decoder,
+	4:  UnpackedRepeatedUint64Decoder,
+	5:  UnpackedRepeatedSint32Decoder,
+	6:  UnpackedRepeatedSint64Decoder,
+	7:  UnpackedRepeatedBoolDecoder,
+	8:  UnpackedRepeatedEnumDecoder,
+	9:  UnpackedRepeatedFixed64Decoder,
+	10: UnpackedRepeatedSfixed64Decoder,
+	11: UnpackedRepeatedDoubleDecoder,
+	12: UnpackedRepeatedFixed32Decoder,
+	13: UnpackedRepeatedSfixed32Decoder,
+	14: UnpackedRepeatedFloatDecoder,
+	15: UnpackedRepeatedStringDecoder,
+	16: UnpackedRepeatedBytesDecoder,
+	17: UnpackedRepeatedMessageDecoder,
+}
+
+var testMapDecoderMap = map[protowire.Number]struct {
+	k keyDecoder
+	v valueDecoder
+}{
+	18: {k: Int32KeyDecoder, v: StringValueDecoder},
+	19: {k: StringKeyDecoder, v: Int32ValueDecoder},
+	20: {k: StringKeyDecoder, v: MessageValueDecoder},
+}
+
 func initTestData(b *testing.B) {
 	var err error
 	testBin, err = proto.Marshal(testMsg)
@@ -80,6 +175,10 @@ func initTestData(b *testing.B) {
 		b.Fatalf("can not marshal Msg message, err: %+v", err)
 	}
 	testPackedRepeatedBin, err = proto.Marshal(testPackedRepeatedMsg)
+	if err != nil {
+		b.Fatalf("can not marshal RepeatedMsgWithPacked message, err: %+v", err)
+	}
+	testUnpackedRepeatedBin, err = proto.Marshal(testUnpackedRepeatedMsg)
 	if err != nil {
 		b.Fatalf("can not marshal RepeatedMsgWithPacked message, err: %+v", err)
 	}
@@ -106,7 +205,7 @@ func BenchmarkDecodeNonRepeatedData(b *testing.B) {
 	}
 }
 
-func BenchmarkRepeatedBaseline(b *testing.B) {
+func BenchmarkPackedRepeatedBaseline(b *testing.B) {
 	initTestData(b)
 	for i := 0; i < b.N; i++ {
 		if err := proto.Unmarshal(testPackedRepeatedBin, &proto3_test.RepeatedMsgWithPacked{}); err != nil {
@@ -130,6 +229,46 @@ func BenchmarkDecodePackedRepeatedData(b *testing.B) {
 			if err != nil {
 				b.Fatalf("decode RepeatedMsgWithPacked %d field failed, err: %+v", tag, err)
 			}
+		}
+	}
+}
+
+func BenchmarkUnpackedRepeatedBaseline(b *testing.B) {
+	initTestData(b)
+	for i := 0; i < b.N; i++ {
+		if err := proto.Unmarshal(testPackedRepeatedBin, &proto3_test.RepeatedMsgWithPacked{}); err != nil {
+			b.Fatalf("can not unmarshal test proto message, err: %+v", err)
+		}
+	}
+}
+
+func BenchmarkDecodeUnpackedRepeatedData(b *testing.B) {
+	initTestData(b)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		m, err := Decode(testUnpackedRepeatedBin, NotSort)
+		if err != nil {
+			b.Fatalf("decode RepeatedMsgWithPacked proto message into ProtoMessage struct failed, err: %+v", err)
+		}
+		// 对所有repeated字段继续解码
+		for i := range m.values {
+			tag := m.values[i].tag
+			if tag == 18 || tag == 19 || tag == 20 {
+				elems, err := m.DecodeMap(tag, testMapDecoderMap[tag].k, testMapDecoderMap[tag].v)
+				if err != nil {
+					b.Fatalf("decode RepeatedMsgWithPacked %d field failed, err: %+v", tag, err)
+				}
+				_, err = FillMapFromProtoMapElem(elems)
+				if err != nil {
+					b.Fatalf("decode RepeatedMsgWithPacked %d field into map failed, err: %+v", tag, err)
+				}
+				continue
+			}
+			_, err := m.DecodeUnpackedRepeated(tag, testUnpackedRepeatedDecoderMap[tag])
+			if err != nil {
+				b.Fatalf("decode RepeatedMsgWithPacked %d field failed, err: %+v", tag, err)
+			}
+
 		}
 	}
 }
